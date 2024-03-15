64e77afe5243d42303511913d6b7c21d75a62d55
Tess Ellenoir Duursma <tess@vanu.dev>
2024-03-15 15:39:25 +0100

    Revamp the library to allow for static initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use std::path::Path;
+use std::path::{PathBuf};
 
 use gcp_auth::{AuthenticationManager, CustomServiceAccount, Token};
+use tokio::sync::OnceCell;
 
 pub enum Error {
     ServiceAccountError,
@@ -8,38 +9,67 @@ pub enum Error {
     EnvironmentError,
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
-
-pub struct Client<'a> {
-    authentication_manager: AuthenticationManager,
-    scopes: &'a [&'a str],
+enum InitializationMethod<'a>
+{
+    File(&'a str),
+    Environment,
 }
 
-impl<'a> Client<'a> {
-    pub async fn from_file<T>(path: T, scopes: &'a [&'a str]) -> Result<Client<'a>>
-    where
-        T: AsRef<Path>,
-    {
-        let service_account =
-            CustomServiceAccount::from_file(path).or(Err(Error::ServiceAccountError))?;
+pub type Result<T> = std::result::Result<T, Error>;
 
-        Ok(Client {
-            authentication_manager: AuthenticationManager::from(service_account),
+pub struct Client<'a>
+{
+    authentication_manager: OnceCell<AuthenticationManager>,
+    scopes: &'a [&'a str],
+    initialization_method: InitializationMethod<'a>,
+}
+
+impl<'a> Client<'a>
+{
+    pub const fn from_file<T>(path: &'a str, scopes: &'a [&'a str]) -> Client<'a>
+    {
+        Client {
+            initialization_method: InitializationMethod::File(path),
+            authentication_manager: OnceCell::const_new(),
             scopes,
-        })
+        }
     }
 
-    pub async fn from_environment(scopes: &'a [&'a str]) -> Result<Client<'a>> {
-        Ok(Client {
-            authentication_manager: AuthenticationManager::new()
-                .await
-                .or(Err(Error::EnvironmentError))?,
+    pub const fn from_environment(scopes: &'a [&'a str]) -> Client<'a> {
+        Client {
+            initialization_method: InitializationMethod::Environment,
+            authentication_manager: OnceCell::const_new(),
             scopes,
+        }
+    }
+
+    async fn get_authentication_manager(&'a self) -> Result<&'a AuthenticationManager> {
+        Ok(match &self.initialization_method {
+            InitializationMethod::File(path) => {
+                self.authentication_manager
+                    .get_or_try_init(|| async {
+                        let service_account = CustomServiceAccount::from_file(PathBuf::from(path))
+                            .or(Err(Error::ServiceAccountError))?;
+
+                        Ok(AuthenticationManager::from(service_account))
+                    })
+                    .await?
+            }
+            InitializationMethod::Environment => {
+                self.authentication_manager
+                    .get_or_try_init(|| async {
+                        Ok(AuthenticationManager::new()
+                            .await
+                            .or(Err(Error::EnvironmentError))?)
+                    })
+                    .await?
+            }
         })
     }
 
     pub async fn get_token(self) -> Result<Token> {
-        self.authentication_manager
+        self.get_authentication_manager()
+            .await?
             .get_token(self.scopes)
             .await
             .or(Err(Error::TokenError))
